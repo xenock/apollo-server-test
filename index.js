ffcbf97f1f5272d61073704729347936d7a0f173
xenock <miguelmoracho@gmail.com>
2020-06-19 13:30:31 +0200

    feat: installed dependencies
    feat: created schema
    
    feat: added rule for allowing comma-dangle and extend standard
    
    feat: added a dataset
    
    feat: resolver defined

---
+++ index.js
@@ -0,0 +1,28 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+]
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+}
